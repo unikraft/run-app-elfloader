73dd1b848109b4ae96b9c9d05694c5c5b3a341f8
Martin Kröning <m.kroening@hotmail.de>
2022-06-26 11:28:00 +0000

    debug.sh: Add support for loading ELF app symbols

--- debug.sh
+++ debug.sh
@@ -1,11 +1,53 @@
 #!/bin/bash
 
-if test $# -ne 1; then
-    echo "Usage: $0 path/to/kvm_image" 1>&2
+usage()
+{
+    echo "Debug QEMU/KVM with ELF Loader app" 1>&2
+    echo ""
+    echo "$0 [-e elf -o offset] path/to/kvm/image" 1>&2
+    echo "    -h - show this help message" 1>&2
+    echo "    -e - set path to ELF app" 1>&2
+    echo "    -o - start address of the ELF app" 1>&2
     exit 1
+}
+
+while getopts "he:o:" OPT; do
+    case ${OPT} in
+        h)
+            usage
+            ;;
+        e)
+            elf=${OPTARG}
+            ;;
+        o)
+            offset=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+shift $((${OPTIND}-1))
+
+if test "$#" -lt 1; then
+    usage
 fi
 
 kvm_image="$1"
+shift
+
+if test ! -z ${elf+x} && test ! -z ${offset+x}; then
+    add_symbol_file="add-symbol-file $elf -o $offset"
+elif test ! -z ${elf+x}; then
+    echo "ELF provided without offset"
+    exit 1
+elif test ! -z ${offset+x}; then
+    echo "offset provided without ELF"
+    exit 1
+else
+    add_symbol_file=""
+fi
 
 # This is the default debug port used by QEMU.
 debug_port="1234"
@@ -23,4 +65,6 @@ gdb --eval-command="target remote :$debug_port" -ex "set confirm off" -ex "set p
     -ex "hbreak _libkvmplat_entry" \
     -ex "c" \
     -ex "disconnect" -ex "set arch i386:x86-64:intel" \
-    -ex "target remote localhost:$debug_port" "$kvm_image"
+    -ex "target remote localhost:$debug_port" \
+    -ex "$add_symbol_file" \
+    "$kvm_image"
