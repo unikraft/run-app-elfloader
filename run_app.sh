e8a31d769674432a123e9fb0b85763069f25a7e7
Razvan Deaconescu <razvand@unikraft.io>
2023-05-14 14:15:16 +0300

    run_app.sh: Add option to explictly use dynamic linker / loader
    Signed-off-by: Razvan Deaconescu <razvand@unikraft.io>

--- run_app.sh
+++ run_app.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+extra_args=""
+
 run_helloworld_static()
 {
     ./run.sh -r rootfs/ ../static-pie-apps/lang/c/helloworld
@@ -74,89 +76,89 @@ run_gzip_static()
 
 run_helloworld()
 {
-    ./run.sh -r ../dynamic-apps/lang/c/ ../dynamic-apps/lang/c/lib64/ld-linux-x86-64.so.2 /helloworld
+    ./run.sh -r ../dynamic-apps/lang/c/ "$extra_args" /helloworld
 }
 
 run_server()
 {
-    ./run.sh -n -r ../dynamic-apps/lang/c/ ../dynamic-apps/lang/c/lib64/ld-linux-x86-64.so.2 /server
+    ./run.sh -n -r ../dynamic-apps/lang/c/ "$extra_args" /server
 }
 
 run_client()
 {
-    ./run.sh -n -r ../dynamic-apps/lang/c/ ../dynamic-apps/lang/c/lib64/ld-linux-x86-64.so.2 /client 172.44.0.1 3333
+    ./run.sh -n -r ../dynamic-apps/lang/c/ "$extra_args" /client 172.44.0.1 3333
 }
 
 run_helloworld_go()
 {
-    ./run.sh -r ../dynamic-apps/lang/go/ ../dynamic-apps/lang/go/lib64/ld-linux-x86-64.so.2 /helloworld
+    ./run.sh -r ../dynamic-apps/lang/go/ "$extra_args" /helloworld
 }
 
 run_server_go()
 {
-    ./run.sh -n -r ../dynamic-apps/lang/go/ ../dynamic-apps/lang/go/lib64/ld-linux-x86-64.so.2 /server
+    ./run.sh -n -r ../dynamic-apps/lang/go/ "$extra_args" /server
 }
 
 run_client_go()
 {
-    ./run.sh -n -r ../dynamic-apps/lang/go/ ../dynamic-apps/lang/go/lib64/ld-linux-x86-64.so.2 /client 172.44.0.1 3333
+    ./run.sh -n -r ../dynamic-apps/lang/go/ "$extra_args" /client 172.44.0.1 3333
 }
 
 run_helloworld_cpp()
 {
-    ./run.sh -r ../dynamic-apps/lang/c++/ ../dynamic-apps/lang/c++/lib64/ld-linux-x86-64.so.2 /helloworld
+    ./run.sh -r ../dynamic-apps/lang/c++/ "$extra_args" /helloworld
 }
 
 run_helloworld_rust()
 {
-    ./run.sh -r ../dynamic-apps/lang/rust/ ../dynamic-apps/lang/rust/lib64/ld-linux-x86-64.so.2 /helloworld
+    ./run.sh -r ../dynamic-apps/lang/rust/ "$extra_args" /helloworld
 }
 
 run_bc()
 {
-    ./run.sh -r ../dynamic-apps/bc/ ../dynamic-apps/bc/lib64/ld-linux-x86-64.so.2 /usr/bin/bc
+    ./run.sh -r ../dynamic-apps/bc/ "$extra_args" /usr/bin/bc
 }
 
 run_gzip()
 {
     echo "test" > ../dynamic-apps/gzip/test.txt
     sudo rm -f ../dynamic-apps/gzip/test.txt.gz
-    ./run.sh -r ../dynamic-apps/gzip/ ../dynamic-apps/gzip/lib64/ld-linux-x86-64.so.2 /bin/gzip test.txt
+    ./run.sh -r ../dynamic-apps/gzip/ "$extra_args" /bin/gzip test.txt
 }
 
 run_sqlite3()
 {
-    ./run.sh -r ../dynamic-apps/sqlite3/ ../dynamic-apps/sqlite3/lib64/ld-linux-x86-64.so.2 /usr/bin/sqlite3
+    ./run.sh -r ../dynamic-apps/sqlite3/ "$extra_args" /usr/bin/sqlite3
 }
 
 run_nginx()
 {
-    ./run.sh -n -r ../dynamic-apps/nginx/ ../dynamic-apps/nginx/lib64/ld-linux-x86-64.so.2 /usr/sbin/nginx
+    ./run.sh -n -r ../dynamic-apps/nginx/ "$extra_args" /usr/local/nginx/sbin/nginx
 }
 
 run_redis()
 {
-    ./run.sh -n -r ../dynamic-apps/redis/ ../dynamic-apps/redis/lib64/ld-linux-x86-64.so.2 /usr/bin/redis-server /etc/redis/redis.conf
+    ./run.sh -n -r ../dynamic-apps/redis/ "$extra_args" /usr/bin/redis-server /etc/redis/redis.conf
 }
 
 run_python()
 {
-    ./run.sh -r ../dynamic-apps/lang/python/ ../dynamic-apps/lang/python/lib64/ld-linux-x86-64.so.2 /bin/python3.11
+    ./run.sh -r ../dynamic-apps/lang/python/ "$extra_args" /bin/python3.11
 }
 
 run_openssl()
 {
-    ./run.sh -r ../dynamic-apps/openssl/ ../dynamic-apps/openssl/lib64/ld-linux-x86-64.so.2 /bin/openssl enc -aes-128-cbc -in /input.txt -out /input.enc -K 01020304050607080900010203040506 -iv 01020304050607080900010203040506
+    ./run.sh -r ../dynamic-apps/openssl/ "$extra_args" /bin/openssl enc -aes-128-cbc -in /input.txt -out /input.enc -K 01020304050607080900010203040506 -iv 01020304050607080900010203040506
 }
 
 run_echo()
 {
-    ./run.sh -r ../dynamic-apps/echo/ ../dynamic-apps/echo/lib64/ld-linux-x86-64.so.2 /bin/echo Hello
+    ./run.sh -r ../dynamic-apps/echo/ "$extra_args" /bin/echo Hello
 }
 
 run_ls()
 {
-    ./run.sh -r ../dynamic-apps/ls/ ../dynamic-apps/ls/lib64/ld-linux-x86-64.so.2 /bin/ls
+    ./run.sh -r ../dynamic-apps/ls/ "$extra_args" /bin/ls
 }
 
 available_applications()
@@ -169,11 +171,17 @@ print_available_apps()
     printf "Possible apps:\n%s" "$(available_applications | tr '\n' ' ' | fold -s)"
 }
 
-if test $# -ne 1; then
-    echo "Usage: $0 <app>"
+if test $# -ne 1 -a $# -ne 2; then
+    echo "Usage: $0 [-l] <app>"
     print_available_apps
+    echo "    -l - use dynamic loader explicitly" 1>&2
     echo 1>&2
     exit 1
+fi
+
+if test "$1" = "-l"; then
+    extra_args="/lib64/ld-linux-x86-64.so.2"
+    shift
 fi
 
 app="$1"
